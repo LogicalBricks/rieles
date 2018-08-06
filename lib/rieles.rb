ec58c1c1172ce9c9432c299ce326feee9e290283
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2018-08-05 23:46:11 -0500

    Remove useless patch

--- lib/rieles.rb
+++ lib/rieles.rb
@@ -1,15 +1,14 @@
 require 'rieles/version'
 require 'rails'
-require 'active_support/inflector/methods_override'
 require 'config/initializer/inflections'
 
 module Rieles
-  I18n.load_path += Dir.glob( File.dirname(__FILE__) + "/config/locales/*.{rb,yml}" ) 
+  I18n.load_path += Dir.glob( File.dirname(__FILE__) + "/config/locales/*.{rb,yml}" )
 
   class Railtie < ::Rails::Railtie
     config.app_generators do |g|
       g.templates.unshift File::expand_path('../templates', __FILE__)
-    end 
+    end
   end
 
   class Engine < ::Rails::Engine
